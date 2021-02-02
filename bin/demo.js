52ed13c0886d0d724d88a2bd3f8a741645f8477f
zhuyuanmin <13058039407@163.com>
2021-02-02 12:10:28 +0800

    Update demo.js

--- bin/demo.js
+++ bin/demo.js
@@ -1,7 +1,6 @@
 #! /usr/bin/env node
 
 const fs = require('fs')
-const path = require('path')
 const program = require('commander')
 const inquirer = require('inquirer')
 const puppeteer = require('puppeteer')
